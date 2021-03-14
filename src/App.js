a8db3e5d470f9f48cc9090a0a676b071c1ac5486
Erik Chacon <erikchacon@me.com>
2021-03-13 18:15:17 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'/brand.png'} className="App-logo" alt="logo" />
+        <p>Sitio Web 02</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nombre: Erik William Chacón Osorio.
         </p>
+        <p>Carnet: 29-0688-1999</p>
+        <p>Materia: Redes II</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://utec.edu.sv"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Universidad Tecnológica
         </a>
       </header>
     </div>
